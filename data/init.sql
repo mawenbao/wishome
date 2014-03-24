e76c7381922f5c28a6f9d3fcbf659b2a73f9d8b7
wilbur ma <mawenbao@hotmail.com>
2014-03-24 22:27:09 +0800

    update init sql and service file

--- data/init.sql
+++ data/init.sql
@@ -2,5 +2,6 @@ drop database if exists wishomedb;
 create database if not exists wishomedb;
 
 grant all privileges on wishomedb.* to wishome@localhost identified by 'wishome';
+grant all privileges on wishomedb.* to wishome@127.0.0.1 identified by 'wishome';
 flush privileges;
 
